7c8228a31c8ddd18b5a0443095f30a394a91a835
J Ulian <hudatov@gmail.com>
2020-07-01 00:04:59 +0300

    bloglist expansion, step3

--- part4/tests/blogs-api.test.js
+++ part4/tests/blogs-api.test.js
@@ -4,6 +4,8 @@ const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
 const helper = require('./test_helper')
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
 
 
 
@@ -140,6 +142,43 @@ test('if the likes property is missing from the request, it will default to the 
     expect(contents[0].likes).toEqual(0)
 
 })
+
+describe('when there is initially one user in db', () => {
+    beforeEach(async () => {
+        await User.deleteMany({})
+
+        const passwordHash = await bcrypt.hash('sekret', 10)
+        const user = new User({
+            username: 'root',
+            passwordHash
+        })
+
+        await user.save()
+    })
+
+    test('creation succeeds with a fresh username', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'mluukkai',
+            name: 'Matti Luukkainen',
+            password: 'salainen',
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+        const usernames = usersAtEnd.map(u => u.username)
+        expect(usernames).toContain(newUser.username)
+    })
+})
+
 
 
 afterAll(() => {
--- part4/tests/test_helper.js
+++ part4/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const blogs = [{
     _id: "5a422a851b54a676234d17f7",
@@ -58,9 +59,14 @@ const blogsInDb = async () => {
     const blogs = await Blog.find({})
     return blogs.map(blog => blog.toJSON())
 }
+const usersInDb = async () => {
+    const users = await User.find({})
+    return users.map(u => u.toJSON())
+}
 
 module.exports = {
     blogs,
     nonExistingId,
-    blogsInDb
+    blogsInDb,
+    usersInDb
 }
