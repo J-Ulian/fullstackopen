8ede60fc99a3a27bb9ae73e59ece1e759ae7f169
J Ulian <hudatov@gmail.com>
2020-07-09 18:59:13 +0300

    ~unicafe revisited, step2

--- part6/unicafe-redux/src/reducer.js
+++ part6/unicafe-redux/src/reducer.js
@@ -9,17 +9,20 @@ const counterReducer = (state = initialState, action) => {
   console.log(action);
   switch (action.type) {
     case 'GOOD':
-      const goodState = { ...state };
-      goodState.good += 1;
-      return goodState;
+      return {
+        ...state,
+        good: state.good + 1,
+      };
     case 'OK':
-      const okState = { ...state };
-      okState.ok += 1;
-      return okState;
+      return {
+        ...state,
+        ok: state.ok + 1,
+      };
     case 'BAD':
-      const badState = { ...state };
-      badState.bad += 1;
-      return badState;
+      return {
+        ...state,
+        bad: state.bad + 1,
+      };
     case 'ZERO':
       return initialState;
   }
