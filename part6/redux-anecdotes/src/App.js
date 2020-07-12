2c3342e14cd4b3519e39883f1be2708f2893cc7c
J Ulian <hudatov@gmail.com>
2020-07-12 16:30:04 +0300

    anecdotes, step6

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,46 +1,12 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { createStore } from 'redux';
-import reducer from './reducers/anecdoteReducer';
-import { createAnecdote, voteFor } from './reducers/anecdoteReducer';
-
-const store = createStore(reducer);
+import AnecdoteForm from './components/AnecdoteForm';
+import AnecdoteList from './components/AnecdoteList';
 
 const App = () => {
-  const addAnecdote = (event) => {
-    event.preventDefault();
-    const content = event.target.anecdote.value;
-    console.log(content);
-    event.target.anecdote.value = '';
-    dispatch(createAnecdote(content));
-  };
-
-  const anecdotes = useSelector((state) => state);
-  const dispatch = useDispatch();
-
-  const vote = (id) => {
-    dispatch(voteFor(id));
-  };
-
   return (
     <div>
-      <h2> Anecdotes </h2>
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div> {anecdote.content} </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}> vote </button>
-          </div>
-        </div>
-      ))}
-      <h2> create new </h2>
-      <form onSubmit={addAnecdote}>
-        <div>
-          <input name="anecdote" />
-        </div>
-        <button type="submit"> create </button>
-      </form>
+      <AnecdoteForm />
+      <AnecdoteList />
     </div>
   );
 };
---
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { voteFor } from '../reducers/anecdoteReducer';
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const byVotes = (b1, b2) => b2.votes - b1.votes;
+  return (
+    <div>
+      <h2> Anecdotes </h2>{' '}
+      {anecdotes.sort(byVotes).map((anecdote) => (
+        <div key={anecdote.id}>
+          <div> {anecdote.content} </div>{' '}
+          <div>
+            has {anecdote.votes}{' '}
+            <button onClick={() => dispatch(voteFor(anecdote.id))}>
+              {' '}
+              vote{' '}
+            </button>{' '}
+          </div>{' '}
+        </div>
+      ))}{' '}
+    </div>
+  );
+};
+
+export default AnecdoteList;
